99c7f23bb48cf5f61649722d720fa01996304325
Cristian Guerrero <cristian1guerrero@hotmail.com>
2022-04-02 16:19:30 -0300

    master

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 /* 
 armar un simulador interactivo la estructura final de tu proyecto integrador
+*/
 
-
-*/alert("simulador interactivo");
+alert("simulador interactivo");
 const ESPACIO =(" ");
 function taza_del_21(){
-    let interes = 21
-    let cualculo_interes = (plata / cuotas * interes / 100);
+    let interes = 21;
+    let cualculo_interes = plata / cuotas * interes / 100;
 }
 let plata = parseInt(prompt("¿cuanto dinero necesitas?"))
 let cuotas = parseInt(prompt("¿Cual es la cantidad de cuotas?"));
 if (cuotas === 12){
-    alert(`tu cuotas mensuales serian de ${ESPACIO} ${taza_del_21}`)
+    alert(`tu cuotas mensuales serian de ${ESPACIO}` + taza_del_21)
     
 }
 else{
