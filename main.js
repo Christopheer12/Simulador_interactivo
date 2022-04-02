bb7afbc8743a0936a0086cfcbffb60f5d12625bc
Cristian Guerrero <cristian1guerrero@hotmail.com>
2022-04-02 16:59:38 -0300

    master

--- main.js
+++ main.js
@@ -10,23 +10,22 @@ let cuotas = parseInt(prompt("¿Cual es la cantidad de cuotas?"));
 let cuotas_por_mes = plata / cuotas;
 let interes = cuotas_por_mes * 21/100;
 let final_por_mes = cuotas_por_mes + interes;
-alert(final_por_mes);
 
 {
     if (cuotas == 12){
-        alert(`tu cuotas mensuales serian de ${ESPACIO}` + taza_del_21)
+        alert(`tu cuotas mensuales serian de ${ESPACIO}` + final_por_mes);
         
     }
     else if(cuotas == 24){
-        alert(`tu cuotas mensuales serian de ${ESPACIO}` + taza_del_21)
+        alert(`tu cuotas mensuales serian de ${ESPACIO}` + taza_del_21);
         
     }
     else if(cuotas == 36){
-        alert(`tu cuotas mensuales serian de ${ESPACIO}` + taza_del_21)
+        alert(`tu cuotas mensuales serian de ${ESPACIO}` + taza_del_21);
         
     }
     else if(cuotas == 48){
-        alert(`tu cuotas mensuales serian de ${ESPACIO}` + taza_del_21)
+        alert(`tu cuotas mensuales serian de ${ESPACIO}` + taza_del_21);
         
     }
     else{
